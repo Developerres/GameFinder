8860f8a9d0f294755f46e734410f941681298391
Oleksandr <devresmail@gmail.com>
2021-09-18 13:48:56 +0200

    add handleSubmit to Search form

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
-import { NavLink } from "react-router-dom";
 import { searchGame } from "../../app/searchSlice";
 import search from "./../../assets/image/icon-search.svg";
 import s from "./Search.module.css";
@@ -12,36 +11,28 @@ const Search = (props) => {
   const dispatch = useDispatch();
   const [searchTerm, setSearchTerm] = useState("");
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(searchGame({ term: searchTerm }));
+    setSearchTerm("");
+    history.push("/searchresult");
+  };
+
   return (
     <div className={s.search}>
       <div>
-        <form onSubmit={(e) => e.preventDefault()} role="search">
+        <form onSubmit={handleSubmit} role="search">
           <input
-            id="search"
             type="search"
             placeholder="Search..."
             autoFocus
             required
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.currentTarget.value)}
-            onKeyPress={(e) => {
-              if (e.key === "Enter") {
-                dispatch(searchGame({ term: searchTerm }));
-                setSearchTerm("");
-                history.push("/searchresult");
-              }
-            }}
           />
-          <NavLink
-            to="/searchresult"
-            className={s.searchSubmit}
-            onClick={() => {
-              dispatch(searchGame({ term: searchTerm }));
-              setSearchTerm("");
-            }}
-          >
+          <button type="submit" className={s.searchSubmit}>
             <img src={search} alt="Search" />
-          </NavLink>
+          </button>
         </form>
       </div>
       <div className={s.searchTitle}>
